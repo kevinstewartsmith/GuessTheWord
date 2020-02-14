ac41a1cd330030170cad308e727f08627fab09a1
kssmith9 <kssmith9@ncsu.edu>
2020-02-14 19:39:41 +0700

    Add orientation code to the app delegate class.

--- AstroHangman/AppDelegate.swift
+++ AstroHangman/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        NotificationCenter.default.addObserver(self, selector: #selector(AppDelegate.rotated), name: UIDevice.orientationDidChangeNotification, object: nil)
         return true
     }
 
@@ -30,6 +31,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    @objc func rotated() {
+        if UIDevice.current.orientation.isLandscape {
+            print("Landscape")
+        }
+
+        if UIDevice.current.orientation.isPortrait {
+            print("Portrait")
+        }
     }
 
 
